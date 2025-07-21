b47de2f54806d05352b0be27e1cbbc813bc7119d
CoderTahmid <tahmid.ibne@gmail.com>
2025-07-21 22:41:20 +0600

    basic structure of Arduino added

---
+++ Notes/Basic structure/Basic_structure/Basic_structure.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // put your setup code here, to run once:
+  /*
+  ei structure ei basically Arduino code ta thake
+  ekhane setup e amra amder device kibhabe set kora ache sheta define kore dei
+  like kon pin theke output and kon pin theke input newa hbe ei bepar gula
+  */
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  /*
+  on the other hand ei loop e amra define kore dei je
+  kon kaj ta bar bar cholbe
+  */
+}
